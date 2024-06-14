73ee9ef494d61e21823b7ab37d020903173cb267
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-14 18:52:44 -0300

    Modulo Amostra: Validacao de modelo UnidadeSaude

--- app/models/unidade_saude.rb
+++ app/models/unidade_saude.rb
@@ -1,3 +1,4 @@
 class UnidadeSaude < ApplicationRecord
-  
+  validates :nome, presence: true 
+  validates :cnes, presence: true
 end
--- spec/factories/unidade_saudes.rb
+++ spec/factories/unidade_saudes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :unidade_saude do
-    nome { "MyString" }
-    cnes { "MyString" }
+    nome { Faker::Name.name_with_middle }
+    cnes { Faker::Alphanumeric.alphanumeric(number: 10)}
   end
 end
