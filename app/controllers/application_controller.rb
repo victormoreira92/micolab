30d623730d4ecbec0bb1f5af1bba5c8579064186
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-14 09:40:42 -0300

    Modulo Usuario: Adicionando o funções ao ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(usuario)
+    session[:usuario_id] = usuario.id
+    home_path
+  end
+
+  def after_sign_out_path_for(usuario)
+    home_index_path
+  end
+
+  def usuario_atual
+    @usuario_atual ||= Usuario.find_by(id: session[:usuario_id])
+  end
+
+  def usuario_logado?
+    usuario_atual.present?
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_12_122217) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_14_123705) do
+  create_table "perfils", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "tipo"
+    t.string "descricao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "usuarios", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
