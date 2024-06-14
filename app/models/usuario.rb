913646306dd98979628e28886fe9651a75e15fe1
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-14 09:46:44 -0300

    Modulo Usuario: Remoção de model Perfil

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,6 @@ class Usuario < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
 end
