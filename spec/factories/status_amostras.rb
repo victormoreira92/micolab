31c4cfb8c54bd0083132c3b4f6376c5da4411caf
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-17 23:46:00 -0300

    Modulo Amostra: Criação factory StatusAmostra

--- spec/factories/status_amostras.rb
+++ spec/factories/status_amostras.rb
@@ -3,7 +3,33 @@ FactoryBot.define do
     situacao { 1 }
     descricao { Faker::Lorem.paragraph }
   end
+
   trait :sem_descricao do 
     descricao { nil }
   end
+
+  trait :sem_status do 
+    situacao { nil }
+  end
+
+  trait :cadastrada do 
+    situacao { 0 }
+    descricao { Faker::Lorem.sentences}
+  end
+
+  trait :processada do 
+    situacao { 1 }
+    descricao { Faker::Lorem.sentences}
+  end
+
+  trait :descartada do 
+    situacao { 2 }
+    descricao { Faker::Lorem.sentences}
+  end
+
+  trait :criopreservada do 
+    situacao { 3 }
+    descricao { Faker::Lorem.sentences}
+  end
+
 end
