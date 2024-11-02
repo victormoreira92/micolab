fdffe1df2226df5ed60e688d9f6c99033f59d538
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-02 13:39:07 -0300

    feat: Remoção de migrations

--- db/migrate/20240614204702_fix_raca.rb
+++
@@ -1,5 +0,0 @@
-class FixRaca < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :pacientes, :raca, :etnia
-  end
-end
--- db/migrate/20240615233944_add_status_amostra_to_amostras.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusAmostraToAmostras < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :amostras, :status_amostra, null: false, foreign_key: true
-  end
-end
