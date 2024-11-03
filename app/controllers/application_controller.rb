6d5ea8e73da8b174b702e9fed15a8d88850f007d
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-03 12:44:12 -0300

    build: Correção de assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action do
-    resource = controller_name.singularize.to_sym
-    method = "#{resource}_params"
-    params[resource] &&= send(method) if respond_to?(method, true)
-  end
+  before_action :authenticate_usuario!
+  skip_before_action :authenticate_usuario!, if: :devise_controller?
+
 
   def after_sign_in_path_for(usuario)
     session[:usuario_id] = usuario.id
@@ -15,8 +13,18 @@ class ApplicationController < ActionController::Base
     home_index_path
   end
 
+  def authenticate_usuario
+    unless usuario_logado?
+      redirect_to home_index_path, alert: 'Você precisa estar logado para acessar esta página.'
+    end
+  end
+
+  def usuario_logado?
+   usuario_atual.present?
+  end
+
   def usuario_atual
-    @usuario_atual ||= Usuario.find_by(id: session[:usuario_id])
+    @usuario_atual ||= Usuario.find_by(id: session[:usuario_id]) if session[:usuario_id]
   end
 
   def usuario_logado?
@@ -28,7 +36,6 @@ class ApplicationController < ActionController::Base
     nome_controller = params[:controller].to_s
     ability_controller = "#{nome_controller.titleize.delete(' ')}Ability".constantize
     @current_ability = ability_controller.new(usuario_atual)
-
   end
 
 end
