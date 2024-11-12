2fe414390ab376c862cd867047b8df34b8c74b93
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-12 11:40:07 -0300

    feat: Criação de abilities

--- app/abilities/tecnico_ability.rb
+++ app/abilities/tecnico_ability.rb
@@ -2,7 +2,7 @@ class TecnicoAbility < PerfilGeralAbility
   attr_accessor :usuario
 
   ABILITIES_POR_CONTROLLER = {
-    "usuarios": 'permissoes_usuarios'
+    "unidades_saudes": 'permissoes_unidades_saudes'
   }.freeze
 
   def initialize(usuario, controller = nil, path = nil, full_path = nil)
@@ -10,6 +10,10 @@ class TecnicoAbility < PerfilGeralAbility
     cannot :index, Usuario
     metodo_permissoes = ABILITIES_POR_CONTROLLER.stringify_keys[controller]
     send(metodo_permissoes) if metodo_permissoes
+  end
+
+  def permissoes_unidades_saudes
+    can :read, UnidadeSaude
   end
 
   
