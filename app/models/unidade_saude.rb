8bb58baf2f263ea05b0e10e2ad4b9eeae4d50603
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-12 11:41:50 -0300

    fix: Correção de paciente_unidade_saude

--- app/models/unidade_saude.rb
+++ app/models/unidade_saude.rb
@@ -1,6 +1,6 @@
 class UnidadeSaude < ApplicationRecord
-  has_many :paciente_unidade_saudes
-  has_many :pacientes, through: :paciente_unidade_saudes
+  has_many :pacientes_unidades_saudes
+  has_many :pacientes, through: :pacientes_unidades_saudes
 
   validates :email_unidade_saude, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :endereco, :nome, :cep, :email_unidade_saude, :telefone,
