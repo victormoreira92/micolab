a1df902615c5858ba7adaf909cb732b70b4991f4
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-17 19:57:47 -0300

    feat: Adição de datattable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import DataTable from 'datatables.net-bs4'
 import * as bootstrap from "bootstrap"
 import jQuery from "jquery";
 import "./datatable"
--- app/javascript/datatable.js
+++ app/javascript/datatable.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-  $("#dttb").dataTable();
+$(document).ready(function () {
+  $('#dttb').DataTable({
+    language: {
+      processing: "Processando...",
+      search: "Pesquisar:",
+      lengthMenu: "Mostrar _MENU_ registros",
+      info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+      infoEmpty: "Mostrando 0 até 0 de 0 registros",
+      infoFiltered: "(filtrado de _MAX_ registros no total)",
+      infoPostFix: "",
+      loadingRecords: "Carregando...",
+      zeroRecords: "Nenhum registro encontrado",
+      emptyTable: "Nenhum dado disponível na tabela",
+      paginate: {
+        first: "Primeiro",
+        previous: "Anterior",
+        next: "Próximo",
+        last: "Último"
+      },
+      aria: {
+        sortAscending: ": ativar para ordenar a coluna em ordem crescente",
+        sortDescending: ": ativar para ordenar a coluna em ordem decrescente"
+      }
+    }
+  });
 });
