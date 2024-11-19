97592f4f7179de4c7d16a3129043399098d6c001
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-19 18:01:05 -0300

    feat: Criação de modelo  de StatusResultado

--- app/models/status_resultado.rb
+++ app/models/status_resultado.rb
@@ -1,2 +1,11 @@
 class StatusResultado < ApplicationRecord
+
+  enum etapa: {
+    cadastrado: 0,
+    em_revisao: 2,
+    liberado: 1,
+    visualizado: 3,
+  }
+
+  validates :etapa, :descricao, presence: true
 end
