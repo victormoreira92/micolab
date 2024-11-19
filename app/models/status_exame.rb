d7b12580ec9ff465113c39c7ffaa2dd76c335d0e
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-18 23:39:05 -0300

    feat: Adição de trait e validação

--- app/models/status_exame.rb
+++ app/models/status_exame.rb
@@ -1,2 +1,12 @@
 class StatusExame < ApplicationRecord
+
+  enum etapa: {
+    em_analise: 0,
+    reprocessamento: 1,
+    realizado: 2,
+    aguardando_nova_amostra: 3,
+    nao_realizado: 4
+  }
+
+  validates :etapa, :descricao, presence: true 
 end
--- spec/factories/status_exames.rb
+++ spec/factories/status_exames.rb
@@ -1,6 +1,26 @@
 FactoryBot.define do
   factory :status_exame do
-    etapa { 1 }
+    etapa { 0 }
     descricao { "MyString" }
+  end
+
+  trait :em_analise do
+    etapa { 0 }
+  end
+
+  trait :reprocessamento do
+    etapa { 1 }
+  end
+
+  trait :realizado do
+    etapa { 2 }
+  end
+
+  trait :aguardando_nova_amostra do
+    etapa { 3 }
+  end
+
+  trait :nao_realizado do
+    etapa { 4 }
   end
 end
