874b2bc0eafcc92c644bb912b2a2e0c31786c17e
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-19 18:01:33 -0300

    test: Criação de teste de model para StatusResultado

--- spec/factories/status_resultados.rb
+++ spec/factories/status_resultados.rb
@@ -3,4 +3,28 @@ FactoryBot.define do
     etapa { 1 }
     descricao { "MyString" }
   end
+
+  trait :cadastrado do
+    etapa { :cadastrado }
+  end
+
+  trait :em_revisao do
+    etapa { :em_revisao }
+  end
+
+  trait :liberado do
+    etapa { :liberado }
+  end
+
+  trait :visualizado do
+    etapa { :visualizado }
+  end
+
+  trait :sem_etapa do
+    etapa { nil }
+  end
+
+  trait :sem_descricao do
+    descricao { nil }
+  end
 end
--- spec/models/status_resultado_spec.rb
+++ spec/models/status_resultado_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe StatusResultado, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validações' do
+    context 'válido' do
+      it 'com todos os campos válidos' do
+        expect { create(:status_resultado) }.to change(described_class, :count).by(1)
+      end
+    end
+
+    context 'não é válido' do
+      it 'sem etapa' do
+        status_resultado = build(:status_resultado, :sem_etapa)
+        expect(status_resultado).not_to be_valid
+      end
+
+      it 'sem descrição' do
+        status_resultado = build(:status_resultado, :sem_descricao)
+        expect(status_resultado).not_to be_valid
+      end
+    end
+  end
 end
