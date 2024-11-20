5be74e0a907876f6601515fd77bf9672a3ea51b1
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-11-20 16:08:26 -0300

    feat: Adição de link de rota Paciente

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,15 @@ Rails.application.routes.draw do
   get 'errors/pagina_nao_encontrada'
   root 'dashboard#index'
 
-
-
   devise_for :usuarios do
     delete 'usuarios/sign_out', :to => 'devise/sessions#destroy', :as => :destroy_usuario_session
   end
   resources :usuarios, except: [:destroy]
 
   resources :unidades_saudes, except: [:destroy]
+  resources :pacientes, except: [:destroy]
 
   get '/403', to: 'errors#acesso_negado'
-
 
   get 'home/index'
 
