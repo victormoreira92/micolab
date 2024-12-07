daa05638b96dec5a3559a32c3047f6b54d244c25
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-12-07 19:18:44 -0300

    feat: Correção ed Javascrit

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,9 @@
 //= link_tree ../builds
 //= link_directoty ../javascripts
 //= link_directory ../stylesheets
-//= link application.js
+//= link bootstrap.min.css
+//= link jquery.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,5 @@
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require select2
+//= require select2_locale_pt-BR
