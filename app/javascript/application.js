dff3aa8b142fe8b9372ae2558f5f131d2c49bf06
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-12-18 13:33:29 -0300

    feat: Adição de plugins Javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,22 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import jQuery from "jquery";
+import Inputmask from "inputmask";
 import "./datatable"
+import "./obter_endereco"
+import "./input_mask"
+import DataTable from 'datatables.net-dt';
+import 'datatables.net-bs4'
+
+
+document.addEventListener('turbo:load', () => {
+  let table = new DataTable('#dttb', {
+    paging: true,
+    responsive: true,
+    columnDefs: [{ width: '30%', targets: 0 }],
+    language: {
+      url: '//cdn.datatables.net/plug-ins/2.1.8/i18n/pt-BR.json',
+    },
+  });
+});
+
