de7a1fda99ecc2c4550b3a3c78829156670eeb86
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-06 20:31:17 -0300

    feat: Criação de Controller Requisição

---
+++ app/controllers/requisicoes_controller.rb
@@ -0,0 +1,24 @@
+class RequisicoesController < ApplicationController
+  load_and_authorize_resource
+  def index
+    @requisicoes = Requisicao.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
