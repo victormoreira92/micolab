96141cd903c2a1e2a58ae329fc5b5191ba1c4eb1
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-06 20:30:03 -0300

    test: Teste de ability sobre Requisição nos perfis

--- spec/abilities/analista_ability_spec.rb
+++ spec/abilities/analista_ability_spec.rb
@@ -13,5 +13,24 @@ RSpec.describe AnalistaAbility, type: :model do
         expect(ability).to be_able_to :read, UnidadeSaude
       end
     end
+    context 'sobre permissões de Requisição' do
+      let(:ability) { described_class.new(usuario_analista, 'requisicoes') }
+
+      it 'pode ler' do
+        expect(ability).to be_able_to :read, Requisicao
+      end
+
+      it 'pode criar' do
+        expect(ability).to be_able_to :create, Requisicao
+      end
+
+      it 'pode editar' do
+        expect(ability).to be_able_to :update, Requisicao
+      end
+
+      it 'pode destruir' do
+        expect(ability).to be_able_to :destroy, Requisicao
+      end
+    end
   end
 end
