c54eb51e2b6bb1aff190727636cf672192679ea1
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-07 00:01:40 -0300

    test: Teste show de request Requisição

--- spec/requests/requisicoes_spec.rb
+++ spec/requests/requisicoes_spec.rb
@@ -2,27 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Requisicoes", type: :request do
   let!(:usuario){ create(:usuario)}
+  let(:paciente){ create(:paciente)}
+  let(:requisicao) { create(:requisicao, paciente: paciente) }
+  let(:informacao_clinica) { create(:informacao_clinica, requisicao_id: requisicao.id)}
+  let(:amostra) { create_list(:amostra, 1, requisicao_id: requisicao.id)}
 
   before do
     post new_usuario_session_path, params: { usuario: { email: usuario.email, password: usuario.password }}
   end
 
   describe "GET /index" do
-    it "retorna success" do
+    it "retornar successo" do
       get requisicoes_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/requisicoes/show"
+    it "retornar sucesso" do
+      requisicao
+      informacao_clinica
+      amostra
+      get requisicao_path(requisicao.id)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
-    it "returns http success" do
+    it "retorno " do
       get "/requisicoes/new"
       expect(response).to have_http_status(:success)
     end
