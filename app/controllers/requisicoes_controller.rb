73ad1d6d454c2ccf6a77885c11a0ecb17ef595d5
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-07 17:55:44 -0300

    feat: Criação de views New e form para requisições

--- app/controllers/requisicoes_controller.rb
+++ app/controllers/requisicoes_controller.rb
@@ -7,9 +7,20 @@ class RequisicoesController < ApplicationController
   def show; end
 
   def new
+    @requisicao = Requisicao.new
   end
 
   def create
+    @requisicao = Requisicao.new(requisicao_params)
+    respond_to do |format|
+      if @requisicao.save
+        flash[:success] = t('activerecord.success.messages.cadastro', model: Requisicao.model_name.human)
+        format.html { redirect_to requisicao_path(@requisicao) }
+      else
+        flash[:error] = @requisicao.errors.full_messages
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -19,5 +30,16 @@ class RequisicoesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def requisicao_params
+    params.require(:requisicao).permit(:numero_requisicao, :paciente_id,
+                                       amostra:[:numero_amostra, :data_coleta, :tipo_amostra, :status_amostra_id, :material_biologico_id],
+                                       informacao_clinica: [:data_inicio_sintomas, :idade_gestacional, :suspeita_diagnostica,
+                                                            :lesao_pulmonar, :tipo_lesao_pulmonar, :localizacao_lesao_pulmonar,
+                                                            :fatores_associados, :uso_antifungicos, :tipo_antifungico,
+                                                            :tipo_atividade_risco, :numero_prontuario, :ocupacao, :caso, :atividade_risco]
+    )
   end
 end
