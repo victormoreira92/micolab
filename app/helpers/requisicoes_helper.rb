47983fd9b485e2311c22351b6575ccd03d09ceeb
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-01-07 18:18:55 -0300

    feat: Adição de InformaçãoClinica em form requisisção

--- app/helpers/requisicoes_helper.rb
+++ app/helpers/requisicoes_helper.rb
@@ -1,3 +1,34 @@
 module RequisicoesHelper
+  def suspeita_diagnostica
+    InformacaoClinica.suspeita_diagnosticas.map{|k, v| [k.humanize.capitalize, v]}
+  end
 
+  def fatores_associados
+    InformacaoClinica.fatores_associados.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def casos
+    InformacaoClinica.casos.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def tipo_antifungicos
+    InformacaoClinica.tipo_antifungicos.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def tipo_lesao_pulmonar
+    InformacaoClinica.tipo_lesao_pulmonars.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def localizacao_lesao_pulmonar
+    InformacaoClinica.localizacao_lesao_pulmonars.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def tipo_atividade_riscos
+    InformacaoClinica.tipo_atividade_riscos.map{|k, v| [k.humanize.capitalize, v]}
+  end
+
+  def zona
+    InformacaoDomiciliar.zonas.map{|k, v| [k.humanize.capitalize, v]}
+  end
 end
+
