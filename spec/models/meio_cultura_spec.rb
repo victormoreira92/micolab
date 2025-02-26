638568a672a6619ea77c0e730b7324e6906fe7ae
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-02-26 09:07:04 -0300

    teste: Criação de modelo MeioCultura

--- spec/models/meio_cultura_spec.rb
+++ spec/models/meio_cultura_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe MeioCultura, type: :model do
       %w[ nome_meio_cultura validade_meio_cultura tipo_meio_cultura
           fabricante_meio_cultura lote_meio_cultura].each do |atributo|
         it "sem #{I18n.t("activerecord.attributes.meio_cultura.#{atributo}")}" do
-        meio_cultura = build(:meio_cultura, "sem_#{atributo}".to_sym)
-        meio_cultura.valid?
+          meio_cultura = build(:meio_cultura, "sem_#{atributo}".to_sym)
+          meio_cultura.valid?
           expect(meio_cultura.errors[atributo.to_sym]).to include(I18n.t('errors.messages.blank'))
         end
       end
