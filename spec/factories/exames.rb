da5d6978481709478931f8ca3157ba1073e55516
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-03-13 14:37:28 -0300

    feat:  Teste de modelo de exame

--- spec/factories/exames.rb
+++ spec/factories/exames.rb
@@ -1,10 +1,23 @@
 FactoryBot.define do
   factory :exame do
-    nome_exame { "MyString" }
-    codigo { "MyString" }
-    metodologia { 1 }
-    descricao { "MyString" }
-    prazo_liberacao { 1 }
-    data_realizacao { "2025-03-13" }
+    nome_exame { Faker::Book.name }
+    codigo { Faker::Alphanumeric.alphanumeric(number: 3, min_alpha: 3) }
+    metodologia { 0 }
+    descricao { Faker::Lorem.sentence }
+    prazo_liberacao { 7 }
+    data_realizacao { nil }
+    association :status_exame
+  end
+
+  trait :sem_nome_exame do
+    nome_exame { nil }
+  end
+
+  trait :sem_metodologia do
+    metodologia { nil }
+  end
+
+  trait :sem_prazo_liberacao do
+    prazo_liberacao { nil }
   end
 end
--- spec/models/exame_spec.rb
+++ spec/models/exame_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Exame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associações' do
+    it { is_expected.to belong_to(:status_exame) }
+    it { is_expected.to have_many(:amostras) }
+  end
+
+  describe 'Validações' do
+    context 'é válido' do
+      it 'com todos os atributos preenchidos' do
+        expect { create(:exame) }.to change(described_class, :count).by(1)
+      end
+    end
+    context 'é inválido' do
+      %w[nome_exame metodologia prazo_liberacao].each do |atributo|
+        it "sem #{I18n.t("activerecord.attributes.exame.#{atributo}")}" do
+          exame = build(:exame, "sem_#{atributo}".to_sym)
+          exame.valid?
+          expect(exame.errors[atributo.to_sym]).to include(I18n.t('errors.messages.blank'))
+        end
+      end
+    end
+  end
 end
